2692b8bd8cf2c12e7ac766960eccc26ff6c623c0
ericromario <83571203+ericromario@users.noreply.github.com>
2023-03-01 23:56:12 +0100

    Update ec2.tf

--- training_terraform/tp_2/ec2.tf
+++ training_terraform/tp_2/ec2.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIARSR2AKWDTGFCW2XO"
-  secret_key = "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"
 }
 
 resource "aws_instance" "monec2" {
